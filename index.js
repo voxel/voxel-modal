add7552cd9edb196081b4e678a45de9eaeba6ad2
deathcap <deathcap@gmx.co.uk>
2013-12-23 19:44:43 -0800

    Add escape keys (press to close)

--- index.js
+++ index.js
@@ -2,23 +2,34 @@
 
 module.exports = Modal;
 
+var ever = require('ever');
+
 function Modal(game, opts) 
 {
   this.game = game;
 
   opts = opts || {};
   this.element = opts.element || (function(){ throw "voxel-modal requires 'element' option" })();
+  this.escapeKeys = opts.escapeKeys || [27];
 
   this.isOpen = false;
 }
 
 Modal.prototype.open = function() {
+  if (this.isOpen) return;
+
   this.game.interact.release();
 
   var self = this;
-  this.game.interact.on('attain', function() {
+  this.game.interact.on('attain', this.onAttain = function() {
     // clicked game, beyond dialog
     self.close();
+  });
+
+  ever(document.body).on('keydown', this.onKeydown = function(ev) {
+    if (self.escapeKeys.indexOf(ev.keyCode) !== -1) {
+      self.close();
+    }
   });
 
   this.element.style.visibility = '';
@@ -26,9 +37,15 @@ Modal.prototype.open = function() {
 };
 
 Modal.prototype.close = function() {
+  if (!this.isOpen) return;
+
+  this.game.interact.removeListener('attain', this.onAttain);
+  ever(document.body).removeListener('keydown', this.onKeydown);
+
   this.element.style.visibility = 'hidden';
 
-  //this.game.interact.request(); // TODO: only request if don't have, or catch error?
+  // resume game interaction
+  this.game.interact.request();
 
   this.isOpen = false;
 };
