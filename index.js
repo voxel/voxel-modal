b0251c525196a9e6d1ad15630862806a187b0a0d
deathcap <deathcap@gmx.co.uk>
2013-12-23 19:10:52 -0800

    Open: release interact, make visible; watch for interact attain. Close: hide

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 // # vim: set shiftwidth=2 tabstop=2 softtabstop=2 expandtab:
 
+module.exports = function(game, opts) {
+  return new Dialog(game, opts);
+};
 
+function Dialog(game, opts) 
+{
+  opts = opts || {};
+  this.div = opts.div || (function(){ throw "voxel-dialog requires 'div' option" })();
+  this.game = game;
+}
+
+Dialog.prototype.open = function() {
+  this.game.interact.release();
+
+  var self = this;
+  this.game.interact.on('attain', function() {
+    // clicked game, beyond dialog
+    this.close();
+  });
+
+  this.div.style.visibility = '';
+};
+
+Dialog.prototype.close = function() {
+  this.div.style.visibility = 'hidden';
+
+  //this.game.interact.request(); // TODO: only request if don't have, or catch error?
+};
